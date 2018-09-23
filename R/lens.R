d88351f7eaeb5fadb9d2dddb468b9f0304306bf8
cfhammill <cfhammill@gmail.com>
2018-09-23 17:02:57 -0400

    Add the oscope feature, sorry Ben

--- R/lens.R
+++ R/lens.R
@@ -16,7 +16,7 @@
 #' @details Lenses are popular in functional programming because
 #' they allow you to build pure, compositional, and re-usable "getters" and "setters".
 #'
-#' An author of a `lens` (via `mkLens`) should ensure it obeys the following
+#' An author of a `lens` (via `lens`) should ensure it obeys the following
 #' rules (the "Lens laws", here paraphrased from
 #' www.schoolofhaskell.com/school/to-infinity-and-beyond/pick-of-the-week/a-little-lens-starter-tutorial):
 #'
@@ -37,11 +37,32 @@
 #'   view(1:10, index(4)) # returns 4
 #'   over(1:10, index(1), 10) # returns c(10, 2:10)
 #' @export
-mkLens <- function(view, over){
+lens <- function(view, over){
   structure(
     list(view = view
        , over = over)
   , class = "lens")
+}
+
+#' Bind data to a lens
+#'
+#' To flatten lens composition, you can prespecify the data
+#' the lens with be applied to by constructing an objectoscope.
+#' These can be integrated easily with normal data pipelines.
+#'
+#' @param d The data for interest
+#' @param l The lens to bind the data to. Defaults to the identity lens
+#' @examples
+#' list(a = 5, b = 1:3, c = 8) %>%
+#'   oscope()   %..%
+#'   index("b") %..%
+#'   index(1)   %>%
+#'   over(10)
+#' @export
+oscope <- function(d, l = idl){
+  structure(
+    list(data = d, lens = l)
+  , class = "oscope")
 }
 
 #' Compose lenses
@@ -56,7 +77,7 @@ mkLens <- function(view, over){
 #' a flipped version of function composition.
 #'
 #' @rdname lens-compose
-#' @param l the first lens
+#' @param l the first [lens] (or an [oscope])
 #' @param m the second lens
 #' @examples
 #'   lst <- list(b = c(3,4,5))
@@ -65,48 +86,92 @@ mkLens <- function(view, over){
 #'   lst %>% over(lns, 1)              # returns list(b = c(3,2,5))
 #'   lst                               # returns list(b = c(3,4,5))
 #' @export
-`%..%` <- function(l, m){
-  if(!inherits(l, "lens") || !inherits(m, "lens"))
-    stop("lens composition is only defined for lenses")
+`%..%` <- function(l, m) UseMethod("%..%")
+
+#' @method "%..%" lens 
+#' @export
+`%..%.lens` <- function(l, m){
+  if(!inherits(m, "lens"))
+    stop("the second argument of lens composition must be a lens")
   
-  mkLens(function(d) m$view(l$view(d))
+  lens(function(d) m$view(l$view(d))
      , over = function(d, x){
        l$over(d, m$over(l$view(d), x))
        })
 }
 
+#' @method "%..%" oscope
+#' @export
+`%..%.oscope` <- function(l, m){
+  if(!inherits(m, "lens"))
+    stop("the second argument of lens composition must be a lens")
+
+  oscope(l$data, l$lens %..% m)
+}
 
 #' Modify data with a lens
 #'
 #' Set the subcomponent of the data referred to by a lens
-#' with a new value. See [mkLens] for details.  Merely dispatches
+#' with a new value. See [lens] for details.  Merely dispatches
 #' to the `over` component of the lens.
 #'
-#' @param d the data
-#' @param l the lens
-#' @param x the replacement value
+#' @param d the data, or an [oscope]
+#' @param l the lens, or in the case of an `oscope`, the replacement
+#' @param x the replacement value, or nothing in the case of an `oscope`
 #' @export
-over <- function(d, l, x){
+over <- function(d,l,x) UseMethod("over", d)
+
+#' @method over default
+#' @export
+over.default <- function(d, l, x){
   if(!inherits(l,"lens"))
-    stop("over is only defined for lenses")
+    stop("second argument of over must be a lens")
 
   l$over(d, x)
+}
+
+#' @method over oscope
+#' @export
+over.oscope <- function(d,l,x){
+  if(!missing("x"))
+    stop("Argument `x` cannot be used with `over` and an `oscope`")
+
+  over(d$data, d$lens, l)
 }
 
 #' View data with a lens
 #'
 #' View the subcomponent of the data referred to by a lens.  This function
-#' merely dispatches to the `view` component of the lens.
+#' merely dispatches to the `view` component of the lens. 
 #'
 #' @param d the data
 #' @param l the lens
 #' @export
-view <- function(d, l){
+view <- function(d, l) UseMethod("view")
+
+#' @method view default
+#' @export
+view.default <- function(d, l){
   if(!inherits(l, "lens"))
     stop("view is only defined for lenses")
 
   l$view(d)
 }
+
+#' @method view oscope
+#' @export
+view.oscope <- function(d, l){
+  if(!missing("l"))
+    stop("Argument `l` cannot be used with `view` and an `oscope`")
+
+  view(d$data, d$lens)
+}
+
+#' The identity (trivial lens)
+#'
+#' This lens focuses on the whole object
+#' @export
+idl <- lens(identity, function(., x) x)
 
 #' Construct a lens into an index/name
 #'
@@ -116,7 +181,7 @@ view <- function(d, l){
 #' can be an `integer` or name.
 #' @export
 index <- function(el){
-  mkLens(view = function(d) d[[el]]
+  lens(view = function(d) d[[el]]
        , over = function(d, x){
          d[[el]] <- x
          d
@@ -131,7 +196,7 @@ index <- function(el){
 #' of `logical`, pointing to one or more elements of the object
 #' @export
 indexes <- function(els){
-  mkLens(view = function(d) d[els]
+  lens(view = function(d) d[els]
        , over = function(d, x){
          d[els] <- x
          d
@@ -142,21 +207,21 @@ indexes <- function(els){
 #'
 #' The lens versions of `names` and `names<-`.
 #' @export
-namel <- mkLens(view = names
+namel <- lens(view = names
               , over = `names<-`)
 
 #' A lens into the column names of an object
 #'
 #' The lens version of `colnames` and `colnames<-`
 #' @export
-colnamesl <- mkLens(view = colnames
+colnamesl <- lens(view = colnames
                   , over = `colnames<-`)
 
 #' A lens into the row names of an object
 #'
 #' The lens version of `rownames` and `rownames<-`
 #' @export
-rownamesl <- mkLens(view = rownames
+rownamesl <- lens(view = rownames
                   , over = `rownames<-`)
 
 #' Construct a lens into an attribute
@@ -166,6 +231,6 @@ rownamesl <- mkLens(view = rownames
 #' the attribute to lens into.
 #' @export
 attrl <- function(attrib){
-  mkLens(view = function(d) attr(d, attrib)
+  lens(view = function(d) attr(d, attrib)
        , over = function(d, x) `attr<-`(d, attrib, x))
 }
