fe8ba479056cc19dbd202bd24b6e38412e02d711
Ben Darwin <bcdarwin@gmail.com>
2018-09-23 12:35:11 -0400

    edit docs and add a couple tiny examples (#1)

--- R/lens.R
+++ R/lens.R
@@ -1,17 +1,24 @@
 #' Construct a lens
 #'
-#' A lens consists of a `view` function and
-#' an `over` function, a getter and setter respectively.
+#' A `lens` represents the process of focussing on a specific part of a data structure.
+#' We represent this via a `view` function and
+#' an `over` function, roughly corresponding to object-oriented
+#' "getters" and "setters" respectively.
 #' Lenses can be composed to access or modify deeply nested
 #' structures.
 #'
-#' @param view A function that takes data and returns a subpart
-#' of that structure
-#' @param over A function that takes data and a value and replaces
-#' the given subpart of the data with that value.
-#' @details lenses are popular in functional programming because
-#' they allow you to compositionally build functional getters and
-#' setters. Lens need to follow the following rules (laws)
+#' @param view A function that takes a data structure of a certain type
+#' and returns a subpart of that structure
+#' @param over A function that takes a data structure of a certain type
+#' and a value and returns a new data structure with the given subpart
+#' replaced with the given value.  Note that `over` should not modify
+#' the original data.
+#' @details Lenses are popular in functional programming because
+#' they allow you to build pure, compositional, and re-usable "getters" and "setters".
+#'
+#' An author of a `lens` (via `mkLens`) should ensure it obeys the following
+#' rules (the "Lenz laws", here paraphrased from
+#' www.schoolofhaskell.com/school/to-infinity-and-beyond/pick-of-the-week/a-little-lens-starter-tutorial):
 #'
 #' 1. Get-Put: If you get (view) some data with a lens, and then
 #' modify (over) the data with that value, you get the input data back.
@@ -20,6 +27,11 @@
 #' 3. Put-Put: If you put a value into some data with a lens, and
 #' then put another value with the same lens, it's the same as only
 #' doing the second put.
+#'
+#' There is nothing particularly new about the lenses appearing here.
+#' For a fairly comprehensive (and rather technical) history of lenses, see
+#' https://github.com/ekmett/lens/wiki/History-of-Lenses and
+#' https://julesh.com/2018/08/16/lenses-for-philosophers/ .
 #'
 #' @examples
 #'   view(1:10, index(4)) # returns 4
@@ -34,14 +46,23 @@ mkLens <- function(view, over){
 
 #' Compose lenses
 #'
-#' Compose two lenses to view or modify a subcomponent of
-#' a subcomponent. Lenses compose such that they are evaluated
-#' left to right, get with the first then get with the second lens,
-#' and set with the first lens the result of setting with the second
-#' lens. See the code for a better explanation.
+#' Compose two lenses to produce a new lens which represents
+#' focussing first with the first lens, then with the second.
+#' A `view` using the resulting composite lens will first `view` using
+#' the first, then the second, while an `over` will `view` via the first lens,
+#' `over` into the resulting piece with the second, and then replace the
+#' updated structure in the first with `over`.  Lens composition
+#' is analogous to the `.` syntax of object-oriented programming or to
+#' a flipped version of function composition.
 #'
 #' @param l the first lens
 #' @param m the second lens
+#' @examples
+#'   lst <- list(b = c(3,4,5))
+#'   lns <- index("b") %..% index(2)
+#'   lst %>% view(lns)                 # returns 4
+#'   lst %>% over(lns, 1)              # returns list(b = c(3,2,5))
+#'   lst                               # returns list(b = c(3,4,5))
 #' @export
 `%..%` <- function(l, m){
   if(!inherits(l, "lens") || !inherits(m, "lens"))
@@ -57,7 +78,8 @@ mkLens <- function(view, over){
 #' Modify data with a lens
 #'
 #' Set the subcomponent of the data referred to by a lens
-#' with a new value. See [mkLens] for details.
+#' with a new value. See [mkLens] for details.  Merely dispatches
+#' to the `over` component of the lens.
 #'
 #' @param d the data
 #' @param l the lens
@@ -72,7 +94,8 @@ over <- function(d, l, x){
 
 #' View data with a lens
 #'
-#' View the subcomponent of the data referred to by a lens.
+#' View the subcomponent of the data referred to by a lens.  This function
+#' merely dispatches to the `view` component of the lens.
 #'
 #' @param d the data
 #' @param l the lens
@@ -119,8 +142,7 @@ indexes <- function(els){
 #' The lens versions of `names` and `names<-`.
 #' @export
 namel <- mkLens(view = names
-              , over = `names<-`
-                )
+              , over = `names<-`)
 
 #' A lens into the column names of an object
 #'
@@ -144,8 +166,5 @@ rownamesl <- mkLens(view = rownames
 #' @export
 attrl <- function(attrib){
   mkLens(view = function(d) attr(d, attrib)
-       , over = function(d,x) `attr<-`(d, attrib, x))
+       , over = function(d, x) `attr<-`(d, attrib, x))
 }
-
-
-  
